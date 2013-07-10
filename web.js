96b4509135228f5475a735a9e5811d1a4dca26b2
Michel Minsoul <mm2@ulg.ac.be>
2013-07-10 07:37:31 +0000

    Updated web.js to read from file

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = requite('fs');
+
+var finename = 'index.html';
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  var msg = fs.readFileSync(filename, 'utf8');
+  response.send(msg);
 });
 
 var port = process.env.PORT || 5000;
